8c994a55a662eedf5bc886e8dedd8ae07dfcff6c
duckpunch <git@duckpunch.org>
2025-12-28 11:27:21 -0800

    Add sequences

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ console.log(`Godash Diagrams v${version}`)
 const textarea = document.querySelector<HTMLTextAreaElement>('#source-input')!
 const initialSource = `problem
 
-A O X E F X .
-B C D O O X .
+a O X e f X .
+b c d O O X .
 O O O X X X .
 X X X . . . .
 . . . . . . .
@@ -16,12 +16,12 @@ X X X . . . .
 size: 19
 to-play: black
 
-paths:
-  F>D>E>B
-  C>B>A
+sequences:
+  f>d>e>b
+  c>b>a
 
 solutions:
-  A>B>D
+  a>b>d
 `
 
 // Set initial value
